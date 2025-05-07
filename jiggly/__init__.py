14122442848779f648ce2a839c9fa8fed1d607bb
LibrarristShalinward <jbh23@mails.tsinghua.edu.cn>
2025-05-08 02:33:13 +0800

    添加 JigglyButton 类并实现基本功能

---
+++ jiggly/__init__.py
@@ -0,0 +1,1 @@
+from .button import JigglyButton as Button
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from jiggly import Button
 import dearpygui.dearpygui as dpg
 import ctypes
 
@@ -7,13 +8,11 @@ ctypes.windll.shcore.SetProcessDpiAwareness(2)
 dpg.create_context()
 
 font_path = r"C:\Users\LibrarristShalinward\AppData\Local\Microsoft\Windows\Fonts\MapleMono-NF-CN-Regular.ttf"
-# font_path = "C:/Windows/Fonts/arial.ttf"
 
 # 加载字体（需要确保路径有效）
 with dpg.font_registry():
     # 参数说明：字体文件路径，字体大小，自定义字体名称
     custom_font = dpg.add_font(font_path, 40, tag = "MapleMono")
-# dpg.set_global_font_scale(.5)
 
 # 新增的窗口背景主题
 with dpg.theme() as window_theme:
@@ -33,34 +32,14 @@ with dpg.theme() as button_theme:
 
 # 创建主窗口
 with dpg.window(label = "Center Button Demo", tag = "Primary Window"):
-    # # 创建用于居中的容器
-    # with dpg.group(horizontal = True):
-    #     dpg.add_spacer(width = 40)  # 左侧间距
-        
-    #     # 垂直居中容器
-    #     with dpg.group():
-    #         dpg.add_spacer()  # 顶部间距
-    #         button = dpg.add_button(
-    #             label = "Click Me", 
-    #             indent = 0, 
-    #             width = 180,       # 按钮宽度
-    #             height = 180,      # 按钮高度（与宽度相同形成方形）
-    #             # pos = (360, 210), 
-    #             tag = "center_btn"
-    #         )
-    #         dpg.set_item_font(button, "MapleMono")
-    #         dpg.add_spacer()  # 底部间距
-        
-    #     dpg.add_spacer(width = 40)  # 右侧间距
-    button = dpg.add_button(
-        label = "Click Me", 
-        indent = 0, 
-        width = 160,       # 按钮宽度
-        height = 160,      # 按钮高度（与宽度相同形成方形）
-        pos = (370, 220), 
+    button = Button(
+        size = 180,
+        pos = (450, 300),
+        theme = button_theme,
+        font = custom_font,
+        label = "Center",
         tag = "center_btn"
-    )
-    dpg.set_item_font(button, "MapleMono")
+    ).item
 
 # 应用自定义主题到按钮
 dpg.bind_item_theme(button, button_theme)
